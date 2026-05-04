37b455de0a663e8c41964c329f1d680b63e0cb91
imane.lmzk <imane.lmzk@gmail.com>
2026-05-04 22:35:52 +0100

    another level

---
+++ React/src/Hooks/useEffectExample.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+export default function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(prev => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <p>Temps : {seconds}s</p>;
+}
